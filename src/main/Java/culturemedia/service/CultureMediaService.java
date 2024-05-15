9b14db1224d15c69edd58f86e65b6d66915ea329
Carlos Zuluaga <carlosandreszuluaga05200@gmail.com>
2024-05-15 07:44:31 -0500

    Interface

---
+++ src/main/Java/culturemedia/service/CultureMediaService.java
@@ -0,0 +1,16 @@
+package culturemedia.service;
+
+import culturemedia.exception.VideoNotFoundException;
+import culturemedia.model.Video;
+import culturemedia.model.View;
+
+import java.util.List;
+
+public interface CultureMediaService {
+    List<Video> findAll() throws VideoNotFoundException;
+    List<Video> find(String title) throws VideoNotFoundException;
+    List<Video> find(Double fromDuration, Double toDuration) throws VideoNotFoundException;
+    Video save(Video video);
+    Video save(List<Video> videos);
+    View save(View view);
+}
